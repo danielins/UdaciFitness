c17e19f94d61ee61b93a7c337146997c183eadfa
daniel <daniel.alves.321@gmail.com>
2017-10-14 14:10:06 -0300

    [core] Lesson 3 finished

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { View } from 'react-native'
-import AddEntry from './components/AddEntry'
+import History from './components/History'
 
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
@@ -11,7 +11,8 @@ export default class App extends React.Component {
     return (
       <Provider store={ createStore(reducer) }>
         <View style={{ flex: 1 }}>
-          <AddEntry />
+          <View style={{height: 20}} />
+          <History />
         </View>
       </Provider>
     )
